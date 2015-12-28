29ceb61e158103392a853f309538ee06c591b3bb
narsenico <not_amon-dev@yahoo.it>
2015-12-28 17:40:01 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 var gulp = require('gulp');
-var debug = require('gulp-debug');
 var serve = require('gulp-serve');
 var watch = require('gulp-watch');
+var htmlmin = require('gulp-htmlmin');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+var gulpSequence = require('gulp-sequence');
+var del = require('del');
+var path = require('path');
+var through = require('through2');
+var chalk = require('chalk');
+var argv = require('yargs')
+    .alias('p', 'port') // serve port
+    .alias('v', 'verbose') // copy verbose
+    .argv;
 
 var jsSrc = [
     'bower_components/bootstrap/dist/js/bootstrap.min.js',
@@ -33,43 +45,161 @@ var fontsSrc = [
     'bower_components/bootstrap/dist/fonts/*'
 ];
 
-gulp.task('copy', function() {
-    gulp.src(jsSrc)
-        .pipe(gulp.dest('dist/js'))
+var debugDefaults = {
+    title: 'debug',
+    verbose: false
+}
+
+function extend(target) {
+    var sources = [].slice.call(arguments, 1);
+    sources.forEach(function(source) {
+        for (var prop in source) {
+            target[prop] = source[prop];
+        }
+    });
+    return target;
+}
+
+function debug(options) {
+    options = extend({}, debugDefaults, options);
+    var count = 0;
+    //
+    return through.obj(
+        function transform(file, enc, cb) {
+            if (options.verbose) {
+                console.log(options.title, ' ', chalk.cyan(path.relative(process.cwd(), file.path)));
+            }
+            count++;
+            cb(null, file);
+        },
+        function flush(cb) {
+            console.log(options.title, ' ', chalk.red(count));
+            cb();
+        });
+}
+
+gulp.task('copy-js', function() {
+    // bower repository
+    return gulp.src(jsSrc)
         .pipe(debug({
-            title: 'js  cp -> '
-        }));
-    gulp.src(cssSrc)
-        .pipe(gulp.dest('dist/css'))
+            title: 'cp js  ->',
+            verbose: argv.v
+        }))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('copy-css', function() {
+    // bower repository
+    return gulp.src(cssSrc)
         .pipe(debug({
-            title: 'css cp -> '
-        }));
+            title: 'cp css ->',
+            verbose: argv.v
+        }))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('copy-fonts', function() {
+    // bower repository
     gulp.src(fontsSrc)
-        .pipe(gulp.dest('dist/fonts'))
         .pipe(debug({
-            title: 'fnt cp -> '
-        }));
-    gulp.src('src/**/*')
+            title: 'cp fnt ->',
+            verbose: argv.v
+        }))
+        .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('copy-src', function() {
+    // local source
+    return gulp.src('src/**/*')
+        .pipe(debug({
+            title: 'cp src ->',
+            verbose: argv.v
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-html', function() {
+    return gulp.src('src/**/*.html')
+        .pipe(debug({
+            title: 'min html ->',
+            verbose: argv.v
+        }))
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-css', function() {
+    return gulp.src(['src/**/*.css', '!src/**/*min*.css'])
+        .pipe(debug({
+            title: 'min css  ->',
+            verbose: argv.v
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(minifyCss())
+        // passo il percorso relativo dove verrà creato il file .map 
+        // altrimenti il contenuto viene scritto direttamente nel file di origine        
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-js', function() {
+    return gulp.src(['src/**/*.js', '!src/**/*min*.js'])
+        .pipe(debug({
+            title: 'ugl js   ->',
+            verbose: argv.v
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        // passo il percorso relativo dove verrà creato il file .map 
+        // altrimenti il contenuto viene scritto direttamente nel file di origine
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist'))
-        .pipe(debug({
-            title: 'src cp -> '
-        }));
+});
+
+gulp.task('clean', function() {
+    return del('dist/*').then(function(paths) {
+        console.log('dist cleaned');
+    });
 });
 
 gulp.task('watch', function() {
     gulp.src('src/**/*')
         .pipe(watch('src/**/*', function(vinyl) {
-            console.log('%s (%s)', vinyl.path, vinyl.event || 'start');
-            }))
+            console.log(chalk.cyan(vinyl.event || 'start'), path.relative(process.cwd() + '/src', vinyl.path));
+        }))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('serve', ['watch'], serve({
+gulp.task('serve', serve({
     root: 'dist',
-    port: 3001
+    port: argv.port || 3001
 }));
 
+gulp.task('copy', gulpSequence('copy-js', 'copy-css', 'copy-fonts', 'copy-src'));
+gulp.task('minify', gulpSequence('minify-html', 'minify-css', 'minify-js'));
+gulp.task('dev', gulpSequence('clean', 'copy', ['watch', 'serve']));
+gulp.task('prod', gulpSequence('clean', 'copy', 'minify'));
+
+// gulp.task('test', function() {
+//     gulp.src('dist/**/*')
+//         .pipe(debug({
+//             title: '**',
+//             verbose: argv.verbose
+//         }))
+//         .pipe(gulp.dest('dist'));
+// });
+
 gulp.task('default', function() {
-    console.log('gulp serve');
-    console.log('gulp copy');
+    console.log('');
+    console.log('gulp', chalk.magenta('watch'), '                 ', chalk.gray(': watch src/'));
+    console.log('gulp', chalk.magenta('serve'), ' ', chalk.yellow('[-p <port>]'), '   ', chalk.gray(': serve dist/'));
+    console.log('gulp', chalk.magenta('copy'), '  ', chalk.yellow('[-v|--verbose]'), '', chalk.gray(': copy src and bower reps into dist/'));
+    console.log('gulp', chalk.magenta('minify'), '', chalk.yellow('[-v|--verbose]'), '', chalk.gray(': minify/uglify html, scripts and css'));
+    console.log('gulp', chalk.magenta('clean'), '                 ', chalk.gray(': clean dist/'));
+    console.log('');
+    console.log('gulp', chalk.magenta('dev'), '   ', chalk.yellow('[-v|--verbose]'), '', chalk.gray(': clean, copy, watch, serve'));
+    console.log('gulp', chalk.magenta('prod'), '  ', chalk.yellow('[-v|--verbose]'), '', chalk.gray(': clean, copy, minify'));
+    console.log('');
 });
